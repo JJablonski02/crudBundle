dc2dea966a39d28ab3fdd5f3a330af5582e37ff0
JJablonski02 <kuba2805@icloud.com>
2023-06-28 00:25:58 +0200

    Created InMemory Database & Integration Test

---
+++ crudTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,33 @@
+﻿using Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace crudTests
+{
+    public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.UseEnvironment("Test");
+
+            builder.ConfigureServices(services =>
+            {
+               var descripter = services.SingleOrDefault(temp => temp.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+
+                if(descripter is not null)
+                {
+                    services.Remove(descripter);
+                }
+                services.AddDbContext<ApplicationDbContext>(options =>
+                {
+                    options.UseInMemoryDatabase("DbForTests");
+                });
+            });
+        }
+    }
+}
---
+++ crudTests/PersonsControllerIntegrationTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System.Net.Http;
+using Xunit;
+
+namespace crudTests
+{
+    public class PersonsControllerIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+    {
+        private readonly HttpClient _client;
+        public PersonsControllerIntegrationTest(CustomWebApplicationFactory factory)
+        {
+            _client = factory.CreateClient();
+        }
+        #region Index
+        [Fact]
+        public async Task Index_ToReturnView()
+        {
+            //Arrange
+            
+
+            //Act
+            HttpResponseMessage response = await _client.GetAsync("/Persons/Index");
+
+            //Assert
+            response.Should().BeSuccessful(); //2xx
+        }
+        #endregion
+    }
+}
