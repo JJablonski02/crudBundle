e8c4e0d8d7aa746cb308b53f60df49bee5c51e0f
JJablonski02 <kuba2805@icloud.com>
2023-08-15 07:59:18 +0200

    ISP services

---
+++ Services/PersonsAdderService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Entities;
+using ServiceContracts.DTO;
+using ServiceContracts;
+using System.ComponentModel.DataAnnotations;
+using Services.Helpers;
+using ServiceContracts.Enums;
+using Microsoft.EntityFrameworkCore;
+using CsvHelper;
+using System.Globalization;
+using CsvHelper.Configuration;
+using OfficeOpenXml;
+using RepositoryContracts;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using SerilogTimings;
+using Exceptions;
+
+namespace Services
+{
+    public class PersonsAdderService : IPersonsAdderService
+    {
+        //private field
+        private readonly IPersonsRepository _personsRepository;
+        private readonly ILogger<PersonsGetterService> _logger;
+        private readonly IDiagnosticContext _diagnosticContext;
+
+        //ctor
+        public PersonsAdderService(IPersonsRepository personsRepository, ILogger<PersonsGetterService> logger, IDiagnosticContext diagnosticContext)
+        {
+            _personsRepository = personsRepository;
+            _logger = logger;
+            _diagnosticContext = diagnosticContext;
+        }
+
+        public async Task<PersonResponse> AddPerson(PersonAddRequest? personAddRequest)
+        {
+            //check if PersonAddRequest is not null
+            if(personAddRequest is null)
+            {
+                throw new ArgumentNullException (nameof(personAddRequest));
+            }
+            //Validate PersonName
+            if (string.IsNullOrEmpty(personAddRequest.PersonName))
+            {
+                throw new ArgumentException("Person cannot be blank");
+            }
+
+            //Model validation 
+            ValidationHelper.ModelValidation(personAddRequest);
+
+            //Convert PersonAddRequest into Person type
+            Person person = personAddRequest.ToPerson();
+
+            //Generate personID
+            person.PersonID = Guid.NewGuid();
+
+            //add person object to persons list
+            await _personsRepository.AddPerson(person);
+
+            //_dbContext.sp_InsertPerson(person);
+
+            //convert the Person object into PersonResponse
+            return person.ToPersonResponse();
+            
+        }
+        
+    }
+}
